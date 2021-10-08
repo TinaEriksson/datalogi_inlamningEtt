b9f14e4d493d1ba5852b015d85d7ab00d34bac9f
Tina Eriksson <65234428+TinaEriksson@users.noreply.github.com>
2021-10-08 19:48:50 +0200

    Add project files.

---
+++ datalogi_inlamningEtt/Menu.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace datalogi_inlamningEtt
+{
+    class Menu
+    {
+        public void MainMenu()
+        {
+                Console.WriteLine("Hello. What do you want to do? \n");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("[1] Check primenumber. \n[2] List primenumbers. " +
+                    "\n[3] Check next primenumber. \n[0] Exit.");
+                Console.ResetColor();
+            Console.Write("Choice: ");
+            var choice = Convert.ToInt32(Console.ReadLine();
+
+            switch(choice)
+            {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 0:
+                    break;
+                default:
+                    Console.WriteLine("Wrong input. Try again!");
+                    break;
+            }
+        }
+
+    }
+}
---
+++ datalogi_inlamningEtt/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace datalogi_inlamningEtt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var menu = new Menu();
+            menu.MainMenu();
+        }
+    }
+}
